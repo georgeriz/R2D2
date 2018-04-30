237dbe9b4321fb2191cecd2226ad307d7f3a156f
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-04-30 23:46:34 +0200

    basic html form and flask form-handler

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template("hello_world.html")
+
+@app.route('/my_input')
+def my_input():
+    return redirect("/")
